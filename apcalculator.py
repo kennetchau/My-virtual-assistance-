53fa12de63be2491922a481dd1ceb35acd8a7d7c
kennetchau <33794042+kennetchau@users.noreply.github.com>
2020-05-14 14:11:04 -0400

    Update apcalculator.py

--- apcalculator.py
+++ apcalculator.py
@@ -5,21 +5,31 @@ import pickle
 
 def cal():
     current = generalfunction.getnumber("What is your current ap? ")
-    target = generalfunction.getnumber("What is your target ap? ")
-    while generalfunction.checklarger(current,target) != True:
-        print("Current ap should be smaller than target ap!")
-        current = generalfunction.getnumber("What is your current ap? ")
-        target = generalfunction.getnumber("What is your target ap? ")
-    difference = float(target) - float(current)
-    timeneededminute = difference * 5
+    difference_40 = 40 - float(current)
+    difference_80 = 80 - float(current)
+    difference_120 = 120 - float(current)
+    timeneededminute_40 = difference_40 * 5
+    timeneededminute_80 = difference_80 * 5
+    timeneededminute_120 = difference_120 * 5
     now = datetime.datetime.now()
-    timeofcompletion = now + datetime.timedelta(hours=0, minutes=timeneededminute, seconds=0)
-    print("Your ap will be replenish to target at " + str(timeofcompletion))
-    pickle.dump(timeofcompletion, open("timeofcompletion.dat","wb"))
+    timeofcompletion_40 = now + datetime.timedelta(hours=0, minutes=timeneededminute_40, seconds=0)
+    timeofcompletion_80 = now + datetime.timedelta(hours=0, minutes=timeneededminute_80, seconds=0)
+    timeofcompletion_120 = now + datetime.timedelta(hours=0, minutes=timeneededminute_120, seconds=0)
+    print("Your ap will be replenish to 40 at " + str(timeofcompletion_40))
+    print("Your ap will be replenish to 80 at " + str(timeofcompletion_80))
+    print("Your ap will be replenish to 120 at " + str(timeofcompletion_120))
+    timeofcompletiondat = open("timeofcompletion.dat","wb")
+    pickle.dump(str(timeofcompletion_40),timeofcompletiondat)
+    pickle.dump(str(timeofcompletion_80), timeofcompletiondat)
+    pickle.dump(str(timeofcompletion_120), timeofcompletiondat)
+    timeofcompletiondat.close()
 
 def showhistory():
-    timeofcompletion = pickle.load(open("timeofcompletion.dat","rb"))
-    print("Your ap will be replenish to target at " + str(timeofcompletion))
+    timeofcompletion = open("timeofcompletion.dat","rb")
+    value1 = pickle.load(timeofcompletion)
+    value2 = pickle.load(timeofcompletion)
+    value3 = pickle.load(timeofcompletion)
+    print("Your ap will be replenish to 40/80/120 at " + str(value1)+", "+str(value2)+", "+str(value3))
 
 
 
