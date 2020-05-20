732da76be3ed6761414019c1f432ba12320dc652
kennetchau <33794042+kennetchau@users.noreply.github.com>
2020-05-20 16:56:52 -0400

    update
    testing new function, calculate the correlation betwen stocks and present it on a heatmap.

--- financeplayground.py
+++ financeplayground.py
@@ -6,6 +6,10 @@ import pandas_datareader as web
 import pickle
 import requests
 import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import style
+style.use('ggplot')
 yf.pdr_override()
 
 ## function to get all ticket of s&p 500
@@ -64,8 +68,29 @@ def compile_data():
     print(main_df.head())
     main_df.to_csv('sp500_joined_closed.csv')
 
+def visualize_data():
+    df = pd.read_csv('sp500_joined_closed.csv')
+    df_corr = df.corr()
+    print(df_corr.head())
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn) #RdYlGn is Red, Yellow, Green (Which mean show the range from red to green)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False) #setting ticks
+    ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+
+    column_labels = df_corr.columns
+    row_labels = df_corr.index
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation = 90)
+    heatmap.set_clim(-1,1)
+    plt.tight_layout()
+    plt.show()
 
 
-
-
-
+visualize_data()
